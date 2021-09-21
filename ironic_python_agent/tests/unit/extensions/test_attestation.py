3521006df55993e340135f83c3e95db49c06ce48
Danni Shi <sdanni@redhat.com>
2021-09-21 09:33:46 -0400

    test

--- ironic_python_agent/tests/unit/extensions/test_attestation.py
+++ ironic_python_agent/tests/unit/extensions/test_attestation.py
@@ -10,16 +10,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unittest import mock
+import shutil
 import tempfile
-import configparser
+from unittest import mock
+
+from oslo_concurrency import processutils
+
 from ironic_python_agent import agent
+from ironic_python_agent import errors
 from ironic_python_agent.extensions import attestation
 from ironic_python_agent.tests.unit import base
-from ironic_python_agent import errors
 from ironic_python_agent import utils
-from oslo_concurrency import processutils
-import shutil
 
 
 @mock.patch.object(utils, 'execute', autospec=True)
@@ -37,22 +38,23 @@ class TestAttestationExtension(base.IronicAgentTest):
     @mock.patch('configparser.ConfigParser.has_option', autospec=True)
     @mock.patch('configparser.ConfigParser.get', autospec=True)
     def test_get_keylime_info(self, mock_cpget,
-            mock_cpoption, mock_cpread, mock_execute):
+                              mock_cpoption, mock_cpread, mock_execute):
         mock_cpread.return_value = None
         mock_cpoption.return_value = True
         mock_cpget.return_value = '9002'
 
         mock_execute.side_effect = [
             ('0x00000001\n', ''),  # get genesis_tpm_handle
-            ('', ''), # tpm2_readpublic
+            ('', ''),  # tpm2_readpublic
             ('uuid_hash_ek ' + self.tmp_dir + '/tpm_ek', ''),  # get hash_ek
         ]
         self.mock_agent.advertise_address = agent.Host('127.0.0.1', 9990)
 
         expected_exec = [
-            mock.call("tpm2_getcap handles-persistent | head -1 | sed 's/- //g'", shell=True),
+            mock.call("tpm2_getcap handles-persistent | head -1"
+                      + " | sed 's/- //g'", shell=True),
             mock.call('tpm2_readpublic', '-c', '0x00000001',
-                '-o', self.tmp_dir + '/tpm_ek', '-f', 'pem'),
+                      '-o', self.tmp_dir + '/tpm_ek', '-f', 'pem'),
             mock.call('sha256sum', self.tmp_dir + '/tpm_ek')
         ]
 
@@ -74,8 +76,12 @@ class TestAttestationExtension(base.IronicAgentTest):
     @mock.patch('configparser.ConfigParser.read', autospec=True)
     @mock.patch('configparser.ConfigParser.has_option', autospec=True)
     @mock.patch('configparser.ConfigParser.get', autospec=True)
-    def test_get_keylime_info_port_error(self, mock_cpget,
-            mock_cpoption, mock_cpread, mock_execute):
+    def test_get_keylime_info_port_error(
+            self,
+            mock_cpget,
+            mock_cpoption,
+            mock_cpread,
+            mock_execute):
         mock_cpread.side_effect = errors.CommandExecutionError('read error')
         async_result = self.agent_extension.get_keylime_info()
         async_result.join()
@@ -86,14 +92,18 @@ class TestAttestationExtension(base.IronicAgentTest):
     @mock.patch('configparser.ConfigParser.read', autospec=True)
     @mock.patch('configparser.ConfigParser.has_option', autospec=True)
     @mock.patch('configparser.ConfigParser.get', autospec=True)
-    def test_get_keylime_info_uuid_error(self, mock_cpget,
-            mock_cpoption, mock_cpread, mock_execute):
+    def test_get_keylime_info_uuid_error(
+            self,
+            mock_cpget,
+            mock_cpoption,
+            mock_cpread,
+            mock_execute):
         mock_cpread.return_value = None
         mock_cpoption.return_value = True
         mock_cpget.return_value = '9002'
         mock_execute.side_effect = [
             ('0x00000001\n', ''),  # get genesis_tpm_handle
-            ('', ''), # tpm2_readpublic
+            ('', ''),  # tpm2_readpublic
             processutils.ProcessExecutionError(
                 stderr='get hash_ek error')  # get hash_ek
         ]
@@ -104,24 +114,29 @@ class TestAttestationExtension(base.IronicAgentTest):
     @mock.patch('configparser.ConfigParser.read', autospec=True)
     @mock.patch('configparser.ConfigParser.has_option', autospec=True)
     @mock.patch('configparser.ConfigParser.get', autospec=True)
-    def test_get_keylime_info_ip_error(self, mock_cpget,
-            mock_cpoption, mock_cpread, mock_execute):
+    def test_get_keylime_info_ip_error(
+            self,
+            mock_cpget,
+            mock_cpoption,
+            mock_cpread,
+            mock_execute):
         mock_cpread.return_value = None
         mock_cpoption.return_value = True
         mock_cpget.return_value = '9002'
 
         mock_execute.side_effect = [
             ('0x00000001\n', ''),  # get genesis_tpm_handle
-            ('', ''), # tpm2_readpublic
+            ('', ''),  # tpm2_readpublic
             ('uuid_hash_ek ' + self.tmp_dir + '/tpm_ek', ''),  # get hash_ek
         ]
         # set the ip to None
         self.mock_agent.advertise_address = agent.Host(None, None)
 
         expected_exec = [
-            mock.call("tpm2_getcap handles-persistent | head -1 | sed 's/- //g'", shell=True),
+            mock.call("tpm2_getcap handles-persistent | head -1"
+                      + " | sed 's/- //g'", shell=True),
             mock.call('tpm2_readpublic', '-c', '0x00000001',
-                '-o', self.tmp_dir + '/tpm_ek', '-f', 'pem'),
+                      '-o', self.tmp_dir + '/tpm_ek', '-f', 'pem'),
             mock.call('sha256sum', self.tmp_dir + '/tpm_ek')
         ]
 
