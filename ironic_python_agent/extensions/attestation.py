0c5e43ae24a38f0694dbd98ef50337bf38697b7b
Danni Shi <sdanni@redhat.com>
2021-09-21 10:41:22 -0400

    remove sleep

--- ironic_python_agent/extensions/attestation.py
+++ ironic_python_agent/extensions/attestation.py
@@ -10,18 +10,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import tempfile
-import os
-from oslo_concurrency import processutils
-from oslo_log import log
 import configparser
 import shutil
+import tempfile
 import time
 
+from oslo_concurrency import processutils
+from oslo_log import log
 
-from ironic_python_agent.extensions import base
 from ironic_python_agent import errors
-
+from ironic_python_agent.extensions import base
 from ironic_python_agent import utils
 
 LOG = log.getLogger(__name__)
@@ -35,8 +33,7 @@ class AttestationExtension(base.BaseAgentExtension):
         :returns: A dict contains ip, uuid, port
         """
         LOG.debug('Getting keylime agent information')
-        # remove this !!!
-        time.sleep(60)
+
         # get keylime-agent's port from config file
         port = None
         try:
@@ -46,7 +43,8 @@ class AttestationExtension(base.BaseAgentExtension):
                 port = config.get(
                     'cloud_agent', 'cloudagent_port')
         except Exception as e:
-            error_msg = ('Getting cloudagent_port failed with an error: %s' % e)
+            error_msg = ('Getting cloudagent_port'
+                         + 'failed with an error: %s' % e)
             LOG.error(error_msg)
             raise errors.CommandExecutionError(error_msg)
 
@@ -61,7 +59,7 @@ class AttestationExtension(base.BaseAgentExtension):
             path_ek = genesis_tpm_temp + "/tpm_ek"
             LOG.debug("ek path is: %s", path_ek)
             utils.execute("tpm2_readpublic", "-c", genesis_tpm_handle.strip(),
-                "-o", path_ek, "-f", "pem")
+                          "-o", path_ek, "-f", "pem")
             hash_ek, _err = utils.execute("sha256sum", path_ek)
             LOG.debug("Get hash_ek:%s", hash_ek)
             uuid = hash_ek.split()[0]
@@ -82,8 +80,8 @@ class AttestationExtension(base.BaseAgentExtension):
             "keylime_agent_port": %s}', uuid, ip, port)
 
         keylime_dict = {"keylime_agent_uuid": uuid,
-                "keylime_agent_ip": ip,
-                "keylime_agent_port": port}
+                        "keylime_agent_ip": ip,
+                        "keylime_agent_port": port}
 
         if any([v is None for v in keylime_dict.values()]):
             msg = 'Incomplete keylime agent information!'
